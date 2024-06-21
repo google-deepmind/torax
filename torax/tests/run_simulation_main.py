5cdd886d6c7f55c7d57128fb93e5ee27663b8755
Ian Goodfellow <goodfellow@google.com>
2024-06-21 08:49:46 -0700

    test that main app runs
    PiperOrigin-RevId: 645402754

--- torax/tests/run_simulation_main.py
+++ torax/tests/run_simulation_main.py
@@ -14,7 +14,10 @@
 
 """Unit tests for torax.run_simulation_main."""
 
+import io
 from unittest import mock
+from absl import app
+from absl import logging
 from absl.testing import absltest
 from absl.testing import flagsaver
 from absl.testing import parameterized
@@ -29,7 +32,7 @@ class RunSimulationMainTest(parameterized.TestCase):
 
   @flagsaver.flagsaver((run_simulation_main._PYTHON_CONFIG_PACKAGE, "torax"))
   def test_import_module(self):
-    """docstring."""
+    """test the import_module function."""
     module = run_simulation_main._import_module(
         ".tests.test_data.test_iterhybrid_newton"
     )
@@ -58,6 +61,35 @@ class RunSimulationMainTest(parameterized.TestCase):
     # a valid "q" for quit.
     user_command = run_simulation_main.prompt_user(config_str)
     self.assertEqual(user_command, run_simulation_main._UserCommand.QUIT)
+
+  @flagsaver.flagsaver(
+      (run_simulation_main._PYTHON_CONFIG_PACKAGE, "torax"),
+      (
+          run_simulation_main._PYTHON_CONFIG_MODULE,
+          ".tests.test_data.test_implicit",
+      ),
+  )
+  @mock.patch("builtins.input", side_effect=["q"])
+  def test_main_app(self, mock_input):
+    """Test that main app runs without errors."""
+    del mock_input  # Needed for @patch interface but not used in this test
+
+    # In this test we run the whole app. We send a mocked 'q' input to quit the
+    # app after it is done running. The app quits an explicit SystemExit that
+    # we need to catch to avoid bringing down the tests. To make sure that the
+    # app really ran as expected, we capture stdout and check that it contains
+    # an expected message from after the successful execution.
+
+    captured_stdout = io.StringIO()
+    handler = logging.PythonHandler(captured_stdout)
+
+    try:
+      logging.get_absl_logger().addHandler(handler)
+      with self.assertRaises(SystemExit):
+        app.run(run_simulation_main.main)
+    finally:
+      logging.get_absl_logger().removeHandler(handler)
+    self.assertIn("Wrote simulation output to", captured_stdout.getvalue())
 
 
 if __name__ == "__main__":
