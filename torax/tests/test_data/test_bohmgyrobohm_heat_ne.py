b34dc5ed5ca8c4bd9b57b10e3ec23fa0f331183a
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2024-08-22 09:21:41 +0100

    Add separate test for heat only

---
+++ torax/tests/test_data/test_bohmgyrobohm_heat_ne.py
@@ -0,0 +1,84 @@
+# Copyright 2024 DeepMind Technologies Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests BgB particle transport.
+
+- BgB transport model
+- Linear stepper with Pereverzev-Corrigan
+- Chi time step calculator
+- CHEASE geometry
+- Sources:
+  - Bootstrap current
+  - jext
+  - NBI
+  - gas puff
+  - pellet
+  - generic ion-el heat source
+  - qei source
+"""
+
+CONFIG = {
+    'runtime_params': {
+        'profile_conditions': {
+            'set_pedestal': True,
+            'nbar': 0.85,  # initial density (Greenwald fraction units)
+            'ne_bound_right': 0.2,
+            'neped': 1.0,
+            # set flat Ohmic current to provide larger range of current
+            # evolution for test
+            'nu': 0,
+        },
+        'numerics': {
+            'ion_heat_eq': True,
+            'el_heat_eq': True,
+            'dens_eq': True,
+            'current_eq': True,
+            'resistivity_mult': 100,  # to shorten current diffusion time
+            't_final': 2,
+        },
+    },
+    'geometry': {
+        'geometry_type': 'chease',
+        'geometry_file': 'ITER_hybrid_citrin_equil_cheasedata.mat2cols',
+        'Ip_from_parameters': True,
+    },
+    'sources': {
+        'generic_ion_el_heat_source': {
+            'w': 0.18202270915319393,
+        },
+        'qei_source': {},
+        'nbi_particle_source': {
+            'S_nbi_tot': 0.3e22,
+        },
+        'gas_puff_source': {
+            'S_puff_tot': 0.5e22,
+        },
+        'pellet_source': {
+            'S_pellet_tot': 1.0e22,
+        },
+        'j_bootstrap': {},
+        'jext': {},
+    },
+    'transport': {
+        'transport_model': 'bohm-gyrobohm',
+    },
+    'stepper': {
+        'stepper_type': 'linear',
+        'predictor_corrector': False,
+        'use_pereverzev': True,
+    },
+    'time_step_calculator': {
+        'calculator_type': 'chi',
+    },
+}
