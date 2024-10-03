0236c2537e7314d3db1b82c21811743c782ba75e
Anushan Fernando <anushanf@google.com>
2024-10-03 11:03:11 -0700

    Create functionality to register sources and remove default source setting code.
    This change enables functionality for sources to be registered without touching TORAX internals.
    
    PiperOrigin-RevId: 681949209

--- torax/config/build_sim.py
+++ torax/config/build_sim.py
@@ -22,9 +22,9 @@ from torax import geometry_provider
 from torax import sim as sim_lib
 from torax.config import config_args
 from torax.config import runtime_params as runtime_params_lib
-from torax.sources import default_sources
 from torax.sources import formula_config
 from torax.sources import formulas
+from torax.sources import register_source
 from torax.sources import runtime_params as source_runtime_params_lib
 from torax.sources import source as source_lib
 from torax.sources import source_models as source_models_lib
@@ -361,9 +361,8 @@ def _build_single_source_builder_from_config(
     extra_init_kwargs: dict[str, Any] | None = None,
 ) -> source_lib.SourceBuilderProtocol:
   """Builds a source builder from the input config."""
-  runtime_params = default_sources.get_default_runtime_params(
-      source_name,
-  )
+  registered_source = register_source.get_registered_source(source_name)
+  runtime_params = registered_source.default_runtime_params_class()
   # Update the defaults with the config provided.
   source_config = copy.copy(source_config)
   if 'mode' in source_config:
@@ -403,7 +402,7 @@ def _build_single_source_builder_from_config(
   assert not isinstance(formula, dict)
   if 'formula' in kwargs:
     assert not isinstance(kwargs['formula'], dict)
-  return default_sources.get_source_builder_type(source_name)(**kwargs)
+  return registered_source.source_builder_class(**kwargs)
   # pylint: enable=missing-kwoa
   # pytype: enable=missing-parameter
 
