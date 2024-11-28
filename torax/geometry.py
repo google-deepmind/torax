Fix duplication and Ip_profile computation

torax/geometry.py
@@ -1377,110 +1377,6 @@ class StandardGeometryIntermediates:
     )
 
   @classmethod
-  def from_IMAS(
-      cls,
-      geometry_dir: str | None = None,
-      geometry_file: str = 'ITER_hybrid_citrin_equil_cheasedata.mat2cols',
-      Ip_from_parameters: bool = False,
-      n_rho: int = 25,
-      hires_fac: int = 4,
-  ) -> StandardGeometryIntermediates:
-    """Constructs a StandardGeometryIntermediates from a IMAS equilibrium IDS.
-
-    Args:
-      geometry_dir: Directory where to find the CHEASE file describing the
-        magnetic geometry. If None, uses the environment variable
-        TORAX_GEOMETRY_DIR if available. If that variable is not set and
-        geometry_dir is not provided, then it defaults to another dir. See
-        implementation.
-      geometry_file: CHEASE file name.
-      Ip_from_parameters: If True, the Ip is taken from the parameters and the
-        values in the Geometry are resacled to match the new Ip.
-      n_rho: Radial grid points (num cells)
-      hires_fac: Grid refinement factor for poloidal flux <--> plasma current
-        calculations.
-
-    Returns:
-      A StandardGeometry instance based on the input file. This can then be
-      used to build a StandardGeometry by passing to `build_standard_geometry`.
-    """
-    #TODO : Write loader function, in geometry_loader ?
-    IMAS_data = geometry_loader.load_geo_data(
-        geometry_dir, geometry_file, geometry_loader.GeometrySource.IMAS
-    ).time_slice[0]
-
-    B0 = np.abs(IMAS_data.global_quantities.magnetic_axis.b_field_tor)
-    Rmaj = IMAS_data.boundary.geometric_axis.r
-
-    # set psi in TORAX units with 2*pi factor
-    psi = -1 * IMAS_data.profiles_1d.psi
-
-    # toroidal flux
-    Phi = IMAS_data.profiles_1d.phi
-
-    # midplane radii
-    Rin = IMAS_data.profiles_1d.r_inboard
-    Rout = IMAS_data.profiles_1d.r_outboard
-    # toroidal field flux function
-    F = np.abs(IMAS_data.profiles_1d.f)
-
-
-    #Flux surface integrals of various geometry quantities
-    #IDS Contour integrals
-    if len(IMAS_data.profiles_1d.dvolume_dpsi) > 0:
-        dvoldpsi = -1 * IMAS_data.profiles_1d.dvolume_dpsi
-    else:
-        dvoldpsi = -1 * np.gradient(IMAS_data.profiles_1d.volume) \
-            / np.gradient(IMAS_data.profiles_1d.psi)
-    if len(IMAS_data.profiles_1d.dpsi_drho_tor) > 0:
-        dpsidrhotor = -1 * IMAS_data.profiles_1d.dpsi_drho_tor
-    else:
-        dpsidrhotor = -1 * np.gradient(IMAS_data.profiles_1d.psi) \
-            / np.gradient(IMAS_data.profiles_1d.rho_tor)
-    flux_surf_avg_RBp = IMAS_data.profiles_1d.gm7 * dpsidrhotor / (2 * np.pi) # dpsi, C0/C1
-    flux_surf_avg_R2Bp2 = IMAS_data.profiles_1d.gm3 * (dpsidrhotor **2) / (4 * np.pi**2) # C4/C1
-    flux_surf_avg_Bp2 = IMAS_data.profiles_1d.gm2 * (dpsidrhotor **2) / (4 * np.pi**2) # C3/C1
-    int_dl_over_Bp = dvoldpsi #C1
-    flux_surf_avg_1_over_R2 = IMAS_data.profiles_1d.gm1 # C2/C1
-
-    #TODO : Read Ip_profile from IMAS_data equilibrium IDS. With IMAS_data.profiles_1d.j_phi we might be able to compute Ip_profile.
-    #Important : j_tor works for IDSs with version < 3.42.0, to replace by j_phi for newer versions.
-    Ip_profile= -1 * IMAS_data.profiles_1d.j_tor
-    rhon = np.sqrt(Phi / Phi[-1])
-    vpr = 4 * np.pi * Phi[-1] * rhon / (F * flux_surf_avg_1_over_R2)
-    # To check
-    z_magnetic_axis = IMAS_data.global_quantities.magnetic_axis.z
-
-
-    return cls(
-        geometry_type=GeometryType.IMAS,
-        Ip_from_parameters=Ip_from_parameters,
-        Rmaj=Rmaj,
-        Rmin=IMAS_data.boundary.minor_radius,
-        B=B0,
-        psi=psi,
-        Ip_profile=Ip_profile,
-        Phi=Phi,
-        Rin=Rin,
-        Rout=Rout,
-        F=F,
-        int_dl_over_Bp=int_dl_over_Bp,
-        flux_surf_avg_1_over_R2=flux_surf_avg_1_over_R2,
-        flux_surf_avg_Bp2=flux_surf_avg_Bp2,
-        flux_surf_avg_RBp=flux_surf_avg_RBp,
-        flux_surf_avg_R2Bp2=flux_surf_avg_R2Bp2,
-        delta_upper_face=IMAS_data.profiles_1d.triangularity_upper,
-        delta_lower_face=IMAS_data.profiles_1d.triangularity_lower,
-        elongation=IMAS_data.profiles_1d.elongation,
-        vpr=vpr,
-        n_rho=n_rho,
-        hires_fac=hires_fac,
-        z_magnetic_axis=z_magnetic_axis,
-    )
-
-
-
-  @classmethod
   def from_IMAS_Data_entry(
       cls,
       geometry_dir: str | None = None,
@@ -1570,12 +1466,17 @@ class StandardGeometryIntermediates:
     int_dl_over_Bp = dvoldpsi #C1
     flux_surf_avg_1_over_R2 = IMAS_data.profiles_1d.gm1 # C2/C1
 
-    #TODO : Read Ip_profile from IMAS_data equilibrium IDS. With IMAS_data.profiles_1d.j_phi we might be able to compute Ip_profile.
+     #TODO : Read Ip_profile from IMAS_data equilibrium IDS. With IMAS_data.profiles_1d.j_phi we might be able to compute Ip_profile.
     #Important : j_tor works for IDSs with version < 3.42.0, to replace by j_phi for newer versions.
-    Ip_profile= -1 * IMAS_data.profiles_1d.j_tor
-
+    #jtor = dI/drhon / (drho/dS) = dI/drhon / spr
+    # spr = vpr / ( 2 * np.pi * Rmaj)
+    # -> Ip_profile = integrate(y = spr * jtor, x= rhon, initial = 0.0)
+    jtor= -1 * IMAS_data.profiles_1d.j_tor
     rhon = np.sqrt(Phi / Phi[-1])
     vpr = 4 * np.pi * Phi[-1] * rhon / (F * flux_surf_avg_1_over_R2)
+    spr = vpr / (2*np.pi * Rmaj)
+    Ip_profile = scipy.integrate.cumulative_trapezoid(y=spr * jtor, x=rhon, initial=0.0)
+
     # To check
     z_magnetic_axis = IMAS_data.global_quantities.magnetic_axis.z
 