3dc15c8f62223fd37d439018ffd2f53cec6b1b86
Sanders Mike EXT <sanderm@sdcc-login03.iter.org>
2025-01-13 10:05:35 +0100

    keep state as original object

--- torax/torax_imastools/util.py
+++ torax/torax_imastools/util.py
@@ -78,7 +78,7 @@ def write_ids_equilibrium_into_config(config: dict, equilibrium)->dict[str,np.nd
   return config
 
 def core_profiles_to_IMAS(ids, state, geometry):
-  """Loads the equilibrium into the geometry config.
+  """Converts torax core_profiles to IMAS IDS.
   Takes the cell grid as a basis and converts values on face grid to cell.
   Args:
   ids: IDS object
@@ -87,7 +87,7 @@ def core_profiles_to_IMAS(ids, state, geometry):
   Returns:
   filled IDS object"""
   t = state.t
-  state = state.core_profiles
+  cp_state = state.core_profiles
   ids.ids_properties.comment = "Grid based on torax cell grid, used cell grid values and interpolated face grid values"
   ids.ids_properties.homogeneous_time = 1
   ids.time = [t]
@@ -99,37 +99,37 @@ def core_profiles_to_IMAS(ids, state, geometry):
   ids.profiles_1d[0].ion[1].element.resize(1)
   ids.vacuum_toroidal_field.r0 = geometry.Rmaj
   ids.vacuum_toroidal_field.b0[0] = geometry.B0
-  ids.global_quantities.current_non_inductive[0] = state.currents.I_bootstrap
+  ids.global_quantities.current_non_inductive[0] = cp_state.currents.I_bootstrap
   ids.profiles_1d[0].grid.rho_tor_norm = geometry.rho_norm
   ids.profiles_1d[0].grid.rho_tor = geometry.rho
-  ids.profiles_1d[0].grid.psi = state.psi.value
+  ids.profiles_1d[0].grid.psi = cp_state.psi.value
   ids.profiles_1d[0].grid.volume = geometry.volume
   ids.profiles_1d[0].grid.area = geometry.area
-  ids.profiles_1d[0].electrons.temperature = state.temp_el.value
-  ids.profiles_1d[0].electrons.density = state.ne.value
-  ids.profiles_1d[0].ion[0].z_ion = state.Zi
-  ids.profiles_1d[0].ion[0].temperature = state.temp_ion.value
-  ids.profiles_1d[0].ion[0].density = state.ni.value
+  ids.profiles_1d[0].electrons.temperature = cp_state.temp_el.value
+  ids.profiles_1d[0].electrons.density = cp_state.ne.value
+  ids.profiles_1d[0].ion[0].z_ion = cp_state.Zi
+  ids.profiles_1d[0].ion[0].temperature = cp_state.temp_ion.value
+  ids.profiles_1d[0].ion[0].density = cp_state.ni.value
   # assume no molecules, revisit later
-  ids.profiles_1d[0].ion[0].element[0].a = state.Zi
-  ids.profiles_1d[0].ion[0].element[0].z_n = state.Ai
-  ids.profiles_1d[0].ion[1].z_ion = state.Zimp
-  ids.profiles_1d[0].ion[1].temperature = state.temp_ion.value
-  ids.profiles_1d[0].ion[1].density = state.nimp.value
+  ids.profiles_1d[0].ion[0].element[0].a = cp_state.Zi
+  ids.profiles_1d[0].ion[0].element[0].z_n = cp_state.Ai
+  ids.profiles_1d[0].ion[1].z_ion = cp_state.Zimp
+  ids.profiles_1d[0].ion[1].temperature = cp_state.temp_ion.value
+  ids.profiles_1d[0].ion[1].density = cp_state.nimp.value
   # assume no molecules, revisit later
-  ids.profiles_1d[0].ion[1].element[0].a = state.Zimp
-  ids.profiles_1d[0].ion[1].element[0].z_n = state.Aimp
-  ids.profiles_1d[0].q = face_to_cell(state.q_face)
-  ids.profiles_1d[0].magnetic_shear = face_to_cell(state.s_face)
-  ids.profiles_1d[0].j_total = state.currents.jtot
-  ids.profiles_1d[0].j_ohmic = state.currents.johm
-  ids.profiles_1d[0].j_non_inductive = state.currents.generic_current_source
-  ids.profiles_1d[0].j_bootstrap = state.currents.j_bootstrap
-  ids.profiles_1d[0].conductivity_parallel = state.currents.sigma
-  # ids. = state.psidot.value
-  # ids. = face_to_cell(state.currents.jtot_face)
-  # ids. = face_to_cell(state.currents.Ip_profile_face)
-  # ids. = face_to_cell(state.currents.j_bootstrap_face)
+  ids.profiles_1d[0].ion[1].element[0].a = cp_state.Zimp
+  ids.profiles_1d[0].ion[1].element[0].z_n = cp_state.Aimp
+  ids.profiles_1d[0].q = face_to_cell(cp_state.q_face)
+  ids.profiles_1d[0].magnetic_shear = face_to_cell(cp_state.s_face)
+  ids.profiles_1d[0].j_total = cp_state.currents.jtot
+  ids.profiles_1d[0].j_ohmic = cp_state.currents.johm
+  ids.profiles_1d[0].j_non_inductive = cp_state.currents.generic_current_source
+  ids.profiles_1d[0].j_bootstrap = cp_state.currents.j_bootstrap
+  ids.profiles_1d[0].conductivity_parallel = cp_state.currents.sigma
+  # ids. = cp_state.psidot.value
+  # ids. = face_to_cell(cp_state.currents.jtot_face)
+  # ids. = face_to_cell(cp_state.currents.Ip_profile_face)
+  # ids. = face_to_cell(cp_state.currents.j_bootstrap_face)
   return ids
 
 # todo check if we can copy form geometry without weird dependency loops
