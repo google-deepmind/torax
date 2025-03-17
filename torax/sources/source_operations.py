381572e5eba8cade16ba5d2cabf092e62471ff49
Anushan Fernando <anushanf@google.com>
2025-03-17 08:15:59 -0700

    Remove calc_and_sum_sources_psi from source_operations.
    This method seems to be unused.
    
    PiperOrigin-RevId: 737620294

--- torax/sources/source_operations.py
+++ torax/sources/source_operations.py
@@ -16,11 +16,7 @@
 import jax
 import jax.numpy as jnp
 from torax import constants
-from torax import state
-from torax.config import runtime_params_slice
 from torax.geometry import geometry
-from torax.sources import source_models as source_models_lib
-from torax.sources import source_profile_builders
 from torax.sources import source_profiles as source_profiles_lib
 
 
@@ -61,43 +57,3 @@ def sum_sources_temp_el(
   """Computes temp_el source values for sim.calc_coeffs."""
   total = sum(source_profiles.temp_el.values())
   return total * geo.vpr
-
-
-def calc_and_sum_sources_psi(
-    static_runtime_params_slice: runtime_params_slice.StaticRuntimeParamsSlice,
-    dynamic_runtime_params_slice: runtime_params_slice.DynamicRuntimeParamsSlice,
-    geo: geometry.Geometry,
-    core_profiles: state.CoreProfiles,
-    source_models: source_models_lib.SourceModels,
-) -> tuple[jax.Array, jax.Array, jax.Array]:
-  """Computes sum of psi sources for psi_dot calculation."""
-
-  # TODO(b/335597108): Revisit how to calculate this once we enable more
-  # expensive source functions that might not jittable (like file-based or
-  # RPC-based sources).
-  j_bootstrap_profiles = source_models.j_bootstrap.get_bootstrap(
-      dynamic_runtime_params_slice=dynamic_runtime_params_slice,
-      static_runtime_params_slice=static_runtime_params_slice,
-      geo=geo,
-      core_profiles=core_profiles,
-  )
-  profiles = source_profiles_lib.SourceProfiles(
-      j_bootstrap=j_bootstrap_profiles,
-      psi={}, temp_el={}, temp_ion={}, ne={},
-      qei=source_profiles_lib.QeiInfo.zeros(geo))
-  source_profile_builders.build_standard_source_profiles(
-      static_runtime_params_slice=static_runtime_params_slice,
-      dynamic_runtime_params_slice=dynamic_runtime_params_slice,
-      geo=geo,
-      core_profiles=core_profiles,
-      source_models=source_models,
-      calculate_anyway=True,
-      psi_only=True,
-      calculated_source_profiles=profiles,
-  )
-
-  return (
-      sum_sources_psi(geo, source_profiles=profiles),
-      j_bootstrap_profiles.sigma,
-      j_bootstrap_profiles.sigma_face,
-  )
