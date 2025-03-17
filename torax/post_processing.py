4460deee137bff6c10456bf5079573dec14a2577
harshit chourasiya <chourasiya400t@gmail.com>
2025-03-17 07:31:59 +0000

    removed effective loss and made dW_th_dt a new output in post_processed_outputs

--- torax/post_processing.py
+++ torax/post_processing.py
@@ -110,9 +110,7 @@ def _calculate_integrated_sources(
       integrated[f'{value}_ion'] = math_utils.volume_integration(
           profile_ion, geo
       )
-      integrated[f'{value}_el'] = math_utils.volume_integration(
-          profile_el, geo
-      )
+      integrated[f'{value}_el'] = math_utils.volume_integration(profile_el, geo)
       integrated[f'{value}_tot'] = (
           integrated[f'{value}_ion'] + integrated[f'{value}_el']
       )
@@ -214,19 +212,16 @@ def make_outputs(
       integrated_sources['P_alpha_tot'] + integrated_sources['P_external_tot']
   )
 
-   # If a previous simulation state is available, compute the time derivative
+  # If a previous simulation state is available, compute the time derivative
   # of the total stored thermal energy (dW_thermal/dt). Otherwise, default to zero.
   if previous_sim_state is not None:
-      dW_th_dt = (W_thermal_tot - previous_sim_state.post_processed_outputs.W_thermal_tot) / sim_state.dt
+    dW_th_dt = (
+        W_thermal_tot - previous_sim_state.post_processed_outputs.W_thermal_tot
+    ) / sim_state.dt
   else:
-      dW_th_dt = 0.0
+    dW_th_dt = 0.0
 
-
-  # Add the dW/dt term to the power loss to account for non-stationary states.
-  effective_loss = Ploss + dW_th_dt
-
-  # TODO(b/380848256): include dW/dt term
-  tauE = W_thermal_tot / effective_loss
+  tauE = W_thermal_tot / Ploss
 
   tauH89P = scaling_laws.calculate_scaling_law_confinement_time(
       geo, sim_state.core_profiles, Ploss, 'H89P'
@@ -347,6 +342,7 @@ def make_outputs(
       q95=q95,
       Wpol=Wpol,
       li3=li3,
+      dW_th_dt=dW_th_dt,  # new output
   )
   # pylint: enable=invalid-name
   return dataclasses.replace(
