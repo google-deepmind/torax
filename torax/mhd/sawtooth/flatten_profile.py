0b9e525c89563c3daa6b141126df25a23756db57
Jonathan Citrin <citrin@google.com>
2025-03-28 10:49:27 -0700

    Extend and generalize flatten_profile for kinetic profiles.
    Now handles both density and temperature redistributions. Temperature redistribution based on energy conservation.
    
    Next PRs:
    1. psi profile modification from current profile flattening
    2. put it all together in a sawtooth crash
    
    PiperOrigin-RevId: 741576460

--- torax/mhd/sawtooth/flatten_profile.py
+++ torax/mhd/sawtooth/flatten_profile.py
@@ -22,37 +22,15 @@ from torax.fvm import cell_variable
 from torax.geometry import geometry
 
 
-def flatten_profile(
+def _get_trial_value(
     rho_norm_q1: array_typing.ScalarFloat,
     rho_norm_mixing: array_typing.ScalarFloat,
     flattening_factor: array_typing.ScalarFloat,
-    original_profile: cell_variable.CellVariable,
+    original_value: array_typing.ArrayFloat,
     geo: geometry.Geometry,
-) -> cell_variable.CellVariable:
-  """Helper to redistribute profiles and conserve integral quantities.
+) -> array_typing.ArrayFloat:
+  """Returns a trial new value using two linear interpolations."""
 
-  This function redistributes a profile due to a sawtooth crash by modifying
-  the profile from the magnetic axis up to the mixing radius. The profile is
-  (roughly) flattened between the magnetic axis and the q=1 surface. Between
-  the q=1 surface to the mixing radius, the profile is linearly redistributed.
-  The original profile value is maintained at the mixing radius.
-
-  The unknown quantity is the value of the redistributed profile at the q=1
-  surface. This is calculated by ensuring that volume integrals are conserved,
-  e.g. for conservation of particles, energy, currents.
-
-  Args:
-    rho_norm_q1: The normalised radius of the q=1 surface.
-    rho_norm_mixing: The normalised radius of the mixing surface.
-    flattening_factor: The factor by which the profile is flattened.
-    original_profile: The original profile to be redistributed.
-    geo: The geometry of the simulation at this time slice.
-
-  Returns:
-    The redistributed profile.
-  """
-
-  original_value = original_profile.value
   rho_norm = geo.rho_norm
   idx_mixing = jnp.searchsorted(rho_norm, rho_norm_mixing, side='left')
 
@@ -80,6 +58,18 @@ def flatten_profile(
   # original_value to avoid unnecessary slicing operations.
   trial_value = jnp.interp(rho_norm, interp_rhos, interp_vals)
 
+  return trial_value, redistribution_mask
+
+
+def _get_scaling_factor(
+    original_value: array_typing.ArrayFloat,
+    trial_value: array_typing.ArrayFloat,
+    value_at_mixing_edge: array_typing.ScalarFloat,
+    redistribution_mask: array_typing.ArrayBool,
+    geo: geometry.Geometry,
+) -> array_typing.ScalarFloat:
+  """Returns a profile scaling factor based on integral conservation."""
+
   # Use where mask for integration regions. Shift by mixing value to
   # calculate correct scaling factor
   shifted_original_integrand = jnp.where(
@@ -93,16 +83,154 @@ def flatten_profile(
   )
   trial_integral = math_utils.volume_integration(shifted_trial_integrand, geo)
 
-  scaling = original_integral / trial_integral
+  return original_integral / trial_integral
+
+
+def flatten_density_profile(
+    rho_norm_q1: array_typing.ScalarFloat,
+    rho_norm_mixing: array_typing.ScalarFloat,
+    flattening_factor: array_typing.ScalarFloat,
+    original_density_profile: cell_variable.CellVariable,
+    geo: geometry.Geometry,
+) -> cell_variable.CellVariable:
+  """Redistributes a density profile while preserving total particle number.
+
+  This function redistributes a profile due to a sawtooth crash by modifying
+  the profile from the magnetic axis up to the mixing radius. The profile is
+  (roughly) flattened between the magnetic axis and the q=1 surface. Between
+  the q=1 surface to the mixing radius, the profile is linearly redistributed.
+  The original profile value is maintained at the mixing radius.
+
+  The unknown quantity is the value of the redistributed profile at the q=1
+  surface. This is calculated by ensuring that volume integrals are conserved,
+  e.g. for conservation of particles, energy, currents.
+
+  Args:
+    rho_norm_q1: The normalised radius of the q=1 surface.
+    rho_norm_mixing: The normalised radius of the mixing surface.
+    flattening_factor: The factor by which the profile is flattened.
+    original_density_profile: The original density profile to be redistributed.
+    geo: The geometry of the simulation at this time slice.
+
+  Returns:
+    The redistributed density profile.
+  """
+
+  original_value = original_density_profile.value
+  rho_norm = geo.rho_norm
+
+  # Get a trial profile shape for the redistributed value and a boolean mask
+  # for the redistribution region.
+  trial_value, redistribution_mask = _get_trial_value(
+      rho_norm_q1,
+      rho_norm_mixing,
+      flattening_factor,
+      original_value,
+      geo,
+  )
+
+  value_at_mixing_edge = jnp.interp(rho_norm_mixing, rho_norm, original_value)
+  scaling = _get_scaling_factor(
+      value_at_mixing_edge=value_at_mixing_edge,
+      original_value=original_value,
+      trial_value=trial_value,
+      redistribution_mask=redistribution_mask,
+      geo=geo,
+  )
 
   # Build the new value using masks and the scaling factor
   new_value = jnp.where(
       redistribution_mask,
-      shifted_trial_integrand * scaling + value_at_mixing_edge,
+      (trial_value - value_at_mixing_edge) * scaling + value_at_mixing_edge,
       original_value,
   )
 
   return dataclasses.replace(
-      original_profile,
+      original_density_profile,
       value=new_value,
+  )
+
+
+def flatten_temperature_profile(
+    rho_norm_q1: array_typing.ScalarFloat,
+    rho_norm_mixing: array_typing.ScalarFloat,
+    flattening_factor: array_typing.ScalarFloat,
+    original_temperature_profile: cell_variable.CellVariable,
+    original_density_profile: cell_variable.CellVariable,
+    flattened_density_profile: cell_variable.CellVariable,
+    geo: geometry.Geometry,
+) -> cell_variable.CellVariable:
+  """Redistributes a temperature profile while preserving total energy.
+
+  The integral of density*temperature is proportional to total energy.
+
+  This function redistributes a profile due to a sawtooth crash by modifying
+  the profile from the magnetic axis up to the mixing radius. The profile is
+  (roughly) flattened between the magnetic axis and the q=1 surface. Between
+  the q=1 surface to the mixing radius, the profile is linearly redistributed.
+  The original profile value is maintained at the mixing radius.
+
+  The unknown quantity is the value of the redistributed profile at the q=1
+  surface. This is calculated by ensuring that volume integrals are conserved,
+  e.g. for conservation of particles, energy, currents.
+
+  Args:
+    rho_norm_q1: The normalised radius of the q=1 surface.
+    rho_norm_mixing: The normalised radius of the mixing surface.
+    flattening_factor: The factor by which the profile is flattened.
+    original_temperature_profile: The original temperature profile to be
+      redistributed.
+    original_density_profile: The original density profile.
+    flattened_density_profile: The already redistributed density profile.
+    geo: The geometry of the simulation at this time slice.
+
+  Returns:
+    The redistributed temperature profile.
+  """
+
+  original_temperature_value = original_temperature_profile.value
+  original_density_value = original_density_profile.value
+  flattened_density_value = flattened_density_profile.value
+  rho_norm = geo.rho_norm
+
+  # Get a trial profile shape for the redistributed value and a boolean mask
+  # for the redistribution region.
+  trial_value, redistribution_mask = _get_trial_value(
+      rho_norm_q1,
+      rho_norm_mixing,
+      flattening_factor,
+      original_temperature_value,
+      geo,
+  )
+
+  pressure_value_at_mixing_edge = jnp.interp(
+      rho_norm_mixing,
+      rho_norm,
+      original_temperature_value * original_density_value,
+  )
+
+  trial_pressure_value = trial_value * flattened_density_value
+
+  scaling = _get_scaling_factor(
+      value_at_mixing_edge=pressure_value_at_mixing_edge,
+      original_value=original_temperature_value * original_density_value,
+      trial_value=trial_pressure_value,
+      redistribution_mask=redistribution_mask,
+      geo=geo,
+  )
+
+  new_pressure_value = (
+      trial_pressure_value - pressure_value_at_mixing_edge
+  ) * scaling + pressure_value_at_mixing_edge
+
+  # Build the new value using masks and the scaling factor
+  new_temperature_value = jnp.where(
+      redistribution_mask,
+      new_pressure_value / flattened_density_value,
+      original_temperature_value,
+  )
+
+  return dataclasses.replace(
+      original_temperature_profile,
+      value=new_temperature_value,
   )
