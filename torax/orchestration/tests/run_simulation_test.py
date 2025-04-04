cc0a3e51d6b179e89dbd34fb3edd02383a8990db
Anushan Fernando <anushanf@google.com>
2025-04-04 04:04:52 -0700

    Unit test for cache hits.
    This change assumes it is a desirable feature of TORAX currently that we don't have any extra compilation occuring when running a simulation for the second time. In this case this is a useful test to add as we have silently added in cache misses to TORAX recently and it is easy to mistakenly add them back in, in future.
    
    It broadly checks if there is any tracing or compilation going on for a second simulation run.
    
    It is prone to internal JAX changing but we will know in either case if it does as the test should fail.
    
    PiperOrigin-RevId: 743889197

--- torax/orchestration/tests/run_simulation_test.py
+++ torax/orchestration/tests/run_simulation_test.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from collections.abc import Sequence
+import logging
 import os
 
 from absl.testing import absltest
@@ -119,6 +120,31 @@ class RunSimulationTest(sim_test_case.SimTestCase):
             )
 
     xr.map_over_datasets(check_equality, datatree_ref, datatree_new)
+
+  def test_no_compile_for_second_run(self):
+    # Access the jax logger and set its level to DEBUG.
+    jax_logger = logging.getLogger('jax')
+    jax_logger.setLevel(logging.DEBUG)
+    with self.assertLogs(logger=jax_logger, level=logging.DEBUG) as l:
+      torax_config = self._get_torax_config('test_iterhybrid_rampup.py')
+      run_simulation.run_simulation(torax_config)
+      # Check that the messages we expect to see for tracing and compilation
+      # are present in the first run.
+      self.assertTrue(any('Finished tracing' in line for line in l.output))
+      self.assertTrue(any('Compiling' in line for line in l.output))
+      self.assertTrue(
+          any('Finished XLA compilation' in line for line in l.output)
+      )
+    with self.assertLogs(jax_logger, level=logging.DEBUG) as l:
+      jax_logger.debug('Second run')
+      torax_config = self._get_torax_config('test_iterhybrid_rampup.py')
+      run_simulation.run_simulation(torax_config)
+      # Check that the same messages are not present in the second run.
+      self.assertFalse(any('Finished tracing' in line for line in l.output))
+      self.assertFalse(any('Compiling f' in line for line in l.output))
+      self.assertFalse(
+          any('Finished XLA compilation' in line for line in l.output)
+      )
 
 
 if __name__ == '__main__':
