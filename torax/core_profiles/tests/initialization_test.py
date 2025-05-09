e9ba405a1c20b5b2d264b18243ff8eaa0e8b923d
Jonathan Citrin <citrin@google.com>
2025-05-09 15:43:36 -0700

    Move, fix, and simplify current initialization test.
    Moved from `tests/state_test.py` to `core_profiles/tests/initialization_test.py` where they belong.
    
    For neoclassical refactor, was helpful to remove external_current_source and j_ohmic from Currents.
    
    This triggered the necessary changes here. In the course of investigating the test, it was found that if using j_tot to calculate j_ohmic within the test, then due to the psi-->j conversion, it was necessary to both significantly increase n_rho, and also reduce the test tolerance.
    
    PiperOrigin-RevId: 756932336

--- torax/core_profiles/tests/initialization_test.py
+++ torax/core_profiles/tests/initialization_test.py
@@ -16,16 +16,23 @@ from unittest import mock
 
 from absl.testing import absltest
 from absl.testing import parameterized
+import jax
 import numpy as np
 from torax import jax_utils
+from torax import math_utils
 from torax.config import build_runtime_params
 from torax.core_profiles import initialization
+from torax.geometry import geometry
 from torax.sources import generic_current_source
+from torax.sources import runtime_params as runtime_params_lib
 from torax.sources import source_models as source_models_lib
+from torax.sources import source_profile_builders
 from torax.sources import source_profiles
 from torax.tests.test_lib import default_configs
 from torax.tests.test_lib import torax_refs
 from torax.torax_pydantic import model_config
+
+# pylint: disable=invalid-name
 
 
 class InitializationTest(parameterized.TestCase):
@@ -77,9 +84,7 @@ class InitializationTest(parameterized.TestCase):
     config = default_configs.get_default_config_dict()
     config['profile_conditions']['psi'] = psi
     torax_config = model_config.ToraxConfig.from_dict(config)
-    source_models = source_models_lib.SourceModels(
-        sources=torax_config.sources
-    )
+    source_models = source_models_lib.SourceModels(sources=torax_config.sources)
     dynamic_runtime_params_slice = (
         build_runtime_params.DynamicRuntimeParamsSliceProvider.from_config(
             torax_config
@@ -99,6 +104,275 @@ class InitializationTest(parameterized.TestCase):
     np.testing.assert_allclose(
         core_profiles.psi.value, expected_psi, atol=1e-6, rtol=1e-6
     )
+
+  @parameterized.parameters([
+      dict(geometry_name='circular'),
+      dict(geometry_name='chease'),
+  ])
+  def test_compare_initial_currents_with_different_initial_j_ohmic(
+      self,
+      geometry_name: str,
+  ):
+    _CURRENT_PROFILE_NU = 2
+    _FRACTION_OF_TOTAL_CURRENT = 0.25
+    _NRHO = 100
+    _TOL = 3e-2
+
+    config = default_configs.get_default_config_dict()
+    config['geometry']['geometry_type'] = geometry_name
+    config['geometry']['n_rho'] = _NRHO
+    config['sources'] = {
+        'j_bootstrap': {
+            'bootstrap_multiplier': 0.0,
+        },
+        'generic_current': {
+            'fraction_of_total_current': _FRACTION_OF_TOTAL_CURRENT
+        },
+    }
+    torax_config = model_config.ToraxConfig.from_dict(config)
+
+    profile_conditions1 = dict(
+        initial_j_is_total_current=True,
+        initial_psi_from_j=True,
+        current_profile_nu=_CURRENT_PROFILE_NU,
+    )
+    profile_conditions2 = dict(
+        initial_j_is_total_current=False,
+        initial_psi_from_j=True,
+        current_profile_nu=_CURRENT_PROFILE_NU,
+    )
+    source_models = source_models_lib.SourceModels(sources=torax_config.sources)
+
+    torax_config.update_fields({'profile_conditions': profile_conditions1})
+    (
+        j_total1,
+        j_total_face1,
+        j_external1,
+        j_ohmic1,
+        _,
+        _,
+    ) = _calculate_currents(torax_config, source_models)
+
+    torax_config.update_fields({'profile_conditions': profile_conditions2})
+    j_total2, j_total_face2, _, j_ohmic2, _, geo = _calculate_currents(
+        torax_config, source_models
+    )
+
+    # calculate total and Ohmic current profile references
+    jformula = (1 - geo.rho_norm**2) ** _CURRENT_PROFILE_NU
+    denom = jax.scipy.integrate.trapezoid(jformula * geo.spr, geo.rho_norm)
+    ctot = torax_config.profile_conditions.Ip.value[0] * 1e6 / denom
+    j_total1_expected = jformula * ctot
+    j_ohmic2_expected = j_total1_expected * (1 - _FRACTION_OF_TOTAL_CURRENT)
+
+    # Due to approximation errors in psi-->jtot conversions, as well as
+    # modifications to jtot on axis, we only compare the current profile
+    # mean values up to relatively loose tolerance.
+
+    # Both total currents should be equal, even if the relative contribution
+    # of Ohmic and external current is different.
+    np.testing.assert_raises(
+        AssertionError,
+        np.testing.assert_allclose,
+        np.mean(j_total1),
+        np.mean(j_total2),
+        rtol=_TOL,
+    )
+
+    # Check that the total current agrees with the expected reference formula.
+    np.testing.assert_allclose(
+        np.mean(j_total1),
+        np.mean(j_total1_expected),
+        rtol=_TOL,
+    )
+
+    # The only non-inductive current is the external current. Therefore the
+    # sum of Ohmic + external current should be equal to the total current.
+    np.testing.assert_allclose(
+        np.mean(j_external1 + j_ohmic1),
+        np.mean(j_total1),
+        rtol=_TOL,
+    )
+
+    # j_ohmic2_expected is the expected formula for j_ohmic when setting
+    # initial_j_is_total_current=False as in Case 2. It is the "nu" formula
+    # scaled down to compensate for the external current.
+    np.testing.assert_allclose(
+        np.mean(j_ohmic2),
+        np.mean(j_ohmic2_expected),
+        rtol=_TOL,
+    )
+
+    # Check that the face conversions agree with the expected reference.
+    np.testing.assert_allclose(
+        np.mean(j_total_face1),
+        np.mean(
+            math_utils.cell_to_face(
+                j_total1_expected,
+                geo,
+                preserved_quantity=math_utils.IntegralPreservationQuantity.SURFACE,
+            )
+        ),
+        rtol=_TOL,
+    )
+
+    np.testing.assert_raises(
+        AssertionError,
+        np.testing.assert_allclose,
+        np.mean(j_total_face2),
+        np.mean(
+            math_utils.cell_to_face(
+                j_total1_expected,
+                geo,
+                preserved_quantity=math_utils.IntegralPreservationQuantity.SURFACE,
+            )
+        ),
+        rtol=_TOL,
+    )
+
+  def test_initial_psi_from_j_with_bootstrap_is_consistent_with_case_without_bootstrap(
+      self,
+  ):
+    _CURRENT_PROFILE_NU = 2
+    _NRHO = 100
+    _TOL = 3e-2
+    config = default_configs.get_default_config_dict()
+    config['geometry']['geometry_type'] = 'chease'
+    config['geometry']['n_rho'] = _NRHO
+    torax_config = model_config.ToraxConfig.from_dict(config)
+
+    source_models = source_models_lib.SourceModels(sources=torax_config.sources)
+
+    profile_conditions1 = dict(
+        initial_j_is_total_current=True,
+        initial_psi_from_j=True,
+        current_profile_nu=_CURRENT_PROFILE_NU,
+    )
+    sources1 = {
+        'j_bootstrap': {
+            'bootstrap_multiplier': 0.0,
+            'mode': runtime_params_lib.Mode.MODEL_BASED,
+        },
+        'generic_current': {
+            'fraction_of_total_current': 0.0,
+            'mode': runtime_params_lib.Mode.MODEL_BASED,
+        },
+    }
+    torax_config.update_fields({
+        'profile_conditions': profile_conditions1,
+        'sources': sources1,
+    })
+    j_total1, _, _, j_ohmic1, _, _ = _calculate_currents(
+        torax_config, source_models
+    )
+
+    profile_conditions2 = dict(
+        initial_j_is_total_current=False,
+        initial_psi_from_j=True,
+        current_profile_nu=_CURRENT_PROFILE_NU,
+    )
+    sources2 = {
+        'j_bootstrap': {
+            'bootstrap_multiplier': 1.0,
+            'mode': runtime_params_lib.Mode.MODEL_BASED,
+        },
+        'generic_current': {
+            'fraction_of_total_current': 0.0,
+            'mode': runtime_params_lib.Mode.MODEL_BASED,
+        },
+    }
+    torax_config.update_fields({
+        'profile_conditions': profile_conditions2,
+        'sources': sources2,
+    })
+
+    _, _, _, j_ohmic2, j_bootstrap2, _ = _calculate_currents(
+        torax_config, source_models
+    )
+
+    # In Case 1, all the current should be Ohmic current.
+    np.testing.assert_allclose(
+        np.mean(j_ohmic1),
+        np.mean(j_total1),
+        rtol=_TOL,
+    )
+
+    # In Case 2, some of the current is bootstrap, so the Ohmic currents should
+    # be different between the two cases.
+    np.testing.assert_raises(
+        AssertionError,
+        np.testing.assert_allclose,
+        np.mean(j_ohmic1),
+        np.mean(j_ohmic2),
+        rtol=_TOL,
+    )
+
+    # The only non-inductive current in Case 2 is the bootstrap current.
+    # Thus, the sum of Ohmic and booststrap currents should be equal to the
+    # total (ohmic) current in Case 1.
+    np.testing.assert_allclose(
+        np.mean(j_total1),
+        np.mean(j_ohmic2 + j_bootstrap2),
+        rtol=_TOL,
+    )
+
+  def test_initial_psi_from_geo_noop_circular(self):
+    """Tests expected behaviour of initial psi and current options."""
+    config = default_configs.get_default_config_dict()
+    config['profile_conditions'] = {
+        'initial_psi_from_j': False,
+    }
+    torax_config = model_config.ToraxConfig.from_dict(config)
+    source_models = source_models_lib.SourceModels(sources=torax_config.sources)
+    jtotal1, _, _, _, _, _ = _calculate_currents(torax_config, source_models)
+
+    torax_config.update_fields({'profile_conditions.initial_psi_from_j': True})
+    jtotal2, _, _, _, _, _ = _calculate_currents(torax_config, source_models)
+
+    np.testing.assert_allclose(jtotal1, jtotal2)
+
+
+def _calculate_currents(
+    torax_config: model_config.ToraxConfig,
+    source_models: source_models_lib.SourceModels,
+) -> tuple[
+    jax.Array, jax.Array, jax.Array, jax.Array, jax.Array, geometry.Geometry
+]:
+  """Calculates jtot, j_external, and j_ohmic currents."""
+
+  static_slice = build_runtime_params.build_static_params_from_config(
+      torax_config
+  )
+
+  dynamic_slice, geo = (
+      build_runtime_params.get_consistent_dynamic_runtime_params_slice_and_geometry(
+          t=torax_config.numerics.t_initial,
+          dynamic_runtime_params_slice_provider=build_runtime_params.DynamicRuntimeParamsSliceProvider.from_config(
+              torax_config
+          ),
+          geometry_provider=torax_config.geometry.build_provider,
+      )
+  )
+
+  core_profiles = initialization.initial_core_profiles(
+      dynamic_runtime_params_slice=dynamic_slice,
+      static_runtime_params_slice=static_slice,
+      geo=geo,
+      source_models=source_models,
+  )
+  core_sources = source_profile_builders.get_all_source_profiles(
+      static_runtime_params_slice=static_slice,
+      dynamic_runtime_params_slice=dynamic_slice,
+      geo=geo,
+      core_profiles=core_profiles,
+      source_models=source_models,
+  )
+  j_total = core_profiles.currents.jtot
+  j_total_face = core_profiles.currents.jtot_face
+  j_external = sum(core_sources.psi.values())
+  j_bootstrap = core_sources.j_bootstrap.j_bootstrap
+  j_ohmic = j_total - j_external - j_bootstrap
+  return j_total, j_total_face, j_external, j_ohmic, j_bootstrap, geo
 
 
 if __name__ == '__main__':
