1f149f9f248ea34f9b9521c940790cc7538d4629
Jonathan Citrin <citrin@google.com>
2025-07-02 13:50:13 -0700

    Add helper functions for components of the sauter bootstrap model.
    Needed since the Angioni-Sauter neoclassical transport model also needs a couple of these terms.
    
    PiperOrigin-RevId: 778630135

--- torax/_src/neoclassical/bootstrap_current/sauter.py
+++ torax/_src/neoclassical/bootstrap_current/sauter.py
@@ -28,6 +28,8 @@ from torax._src.neoclassical.bootstrap_current import base
 from torax._src.neoclassical.bootstrap_current import runtime_params
 from torax._src.physics import collisions
 
+# pylint: disable=invalid-name
+
 
 @jax.tree_util.register_dataclass
 @dataclasses.dataclass(frozen=True)
@@ -140,69 +142,10 @@ def _calculate_bootstrap_current(
   )
 
   # Calculate terms needed for bootstrap current
-  denom = (
-      1.0
-      + (1 - 0.1 * f_trap) * jnp.sqrt(nu_e_star)
-      + 0.5 * (1.0 - f_trap) * nu_e_star / Z_eff_face
-  )
-  ft31 = f_trap / denom
-  ft32ee = f_trap / (
-      1
-      + 0.26 * (1 - f_trap) * jnp.sqrt(nu_e_star)
-      + 0.18 * (1 - 0.37 * f_trap) * nu_e_star / jnp.sqrt(Z_eff_face)
-  )
-  ft32ei = f_trap / (
-      1
-      + (1 + 0.6 * f_trap) * jnp.sqrt(nu_e_star)
-      + 0.85 * (1 - 0.37 * f_trap) * nu_e_star * (1 + Z_eff_face)
-  )
-  ft34 = f_trap / (
-      1.0
-      + (1 - 0.1 * f_trap) * jnp.sqrt(nu_e_star)
-      + 0.5 * (1.0 - 0.5 * f_trap) * nu_e_star / Z_eff_face
-  )
-
-  F32ee = (
-      (0.05 + 0.62 * Z_eff_face)
-      / (Z_eff_face * (1 + 0.44 * Z_eff_face))
-      * (ft32ee - ft32ee**4)
-      + 1
-      / (1 + 0.22 * Z_eff_face)
-      * (ft32ee**2 - ft32ee**4 - 1.2 * (ft32ee**3 - ft32ee**4))
-      + 1.2 / (1 + 0.5 * Z_eff_face) * ft32ee**4
-  )
-
-  F32ei = (
-      -(0.56 + 1.93 * Z_eff_face)
-      / (Z_eff_face * (1 + 0.44 * Z_eff_face))
-      * (ft32ei - ft32ei**4)
-      + 4.95
-      / (1 + 2.48 * Z_eff_face)
-      * (ft32ei**2 - ft32ei**4 - 0.55 * (ft32ei**3 - ft32ei**4))
-      - 1.2 / (1 + 0.5 * Z_eff_face) * ft32ei**4
-  )
-
-  term_0 = (1 + 1.4 / (Z_eff_face + 1)) * ft31
-  term_1 = -1.9 / (Z_eff_face + 1) * ft31**2
-  term_2 = 0.3 / (Z_eff_face + 1) * ft31**3
-  term_3 = 0.2 / (Z_eff_face + 1) * ft31**4
-  L31 = term_0 + term_1 + term_2 + term_3
-
-  L32 = F32ee + F32ei
-
-  L34 = (
-      (1 + 1.4 / (Z_eff_face + 1)) * ft34
-      - 1.9 / (Z_eff_face + 1) * ft34**2
-      + 0.3 / (Z_eff_face + 1) * ft34**3
-      + 0.2 / (Z_eff_face + 1) * ft34**4
-  )
-
-  alpha0 = -1.17 * (1 - f_trap) / (1 - 0.22 * f_trap - 0.19 * f_trap**2)
-  alpha = (
-      (alpha0 + 0.25 * (1 - f_trap**2) * jnp.sqrt(nu_i_star))
-      / (1 + 0.5 * jnp.sqrt(nu_i_star))
-      + 0.315 * nu_i_star**2 * f_trap**6
-  ) / (1 + 0.15 * nu_i_star**2 * f_trap**6)
+  L31 = _calculate_L31(f_trap, nu_e_star, Z_eff_face)
+  L32 = _calculate_L32(f_trap, nu_e_star, Z_eff_face)
+  L34 = _calculate_L34(f_trap, nu_e_star, Z_eff_face)
+  alpha = _calculate_alpha(f_trap, nu_i_star)
 
   # calculate bootstrap current
   prefactor = -geo.F_face * bootstrap_multiplier * 2 * jnp.pi / geo.B_0
@@ -236,3 +179,95 @@ def _calculate_bootstrap_current(
       j_bootstrap=j_bootstrap,
       j_bootstrap_face=j_bootstrap_face,
   )
+
+
+def _calculate_L31(
+    f_trap: chex.Array,
+    nu_e_star: chex.Array,
+    Z_eff: chex.Array,
+) -> chex.Array:
+  """Calculates the L31 coefficient: Sauter PoP 1999 Eqs. 14a + 14b."""
+  denom = (
+      1.0
+      + (1 - 0.1 * f_trap) * jnp.sqrt(nu_e_star)
+      + 0.5 * (1.0 - f_trap) * nu_e_star / Z_eff
+  )
+  ft31 = f_trap / denom
+  term_0 = (1 + 1.4 / (Z_eff + 1)) * ft31
+  term_1 = -1.9 / (Z_eff + 1) * ft31**2
+  term_2 = 0.3 / (Z_eff + 1) * ft31**3
+  term_3 = 0.2 / (Z_eff + 1) * ft31**4
+  return term_0 + term_1 + term_2 + term_3
+
+
+def _calculate_L32(
+    f_trap: chex.Array,
+    nu_e_star: chex.Array,
+    Z_eff: chex.Array,
+) -> chex.Array:
+  """Calculates the L32 coefficient: Sauter PoP 1999 Eqs. 15a-e."""
+  # pylint: disable=invalid-name
+  ft32ee = f_trap / (
+      1
+      + 0.26 * (1 - f_trap) * jnp.sqrt(nu_e_star)
+      + 0.18 * (1 - 0.37 * f_trap) * nu_e_star / jnp.sqrt(Z_eff)
+  )
+  ft32ei = f_trap / (
+      1
+      + (1 + 0.6 * f_trap) * jnp.sqrt(nu_e_star)
+      + 0.85 * (1 - 0.37 * f_trap) * nu_e_star * (1 + Z_eff)
+  )
+
+  F32ee = (
+      (0.05 + 0.62 * Z_eff)
+      / (Z_eff * (1 + 0.44 * Z_eff))
+      * (ft32ee - ft32ee**4)
+      + 1
+      / (1 + 0.22 * Z_eff)
+      * (ft32ee**2 - ft32ee**4 - 1.2 * (ft32ee**3 - ft32ee**4))
+      + 1.2 / (1 + 0.5 * Z_eff) * ft32ee**4
+  )
+
+  F32ei = (
+      -(0.56 + 1.93 * Z_eff)
+      / (Z_eff * (1 + 0.44 * Z_eff))
+      * (ft32ei - ft32ei**4)
+      + 4.95
+      / (1 + 2.48 * Z_eff)
+      * (ft32ei**2 - ft32ei**4 - 0.55 * (ft32ei**3 - ft32ei**4))
+      - 1.2 / (1 + 0.5 * Z_eff) * ft32ei**4
+  )
+  return F32ee + F32ei
+
+
+def _calculate_L34(
+    f_trap: chex.Array,
+    nu_e_star: chex.Array,
+    Z_eff: chex.Array,
+) -> chex.Array:
+  """Calculates the L34 coefficient: Sauter PoP 1999 Eqs. 16a+b."""
+  ft34 = f_trap / (
+      1.0
+      + (1 - 0.1 * f_trap) * jnp.sqrt(nu_e_star)
+      + 0.5 * (1.0 - 0.5 * f_trap) * nu_e_star / Z_eff
+  )
+  return (
+      (1 + 1.4 / (Z_eff + 1)) * ft34
+      - 1.9 / (Z_eff + 1) * ft34**2
+      + 0.3 / (Z_eff + 1) * ft34**3
+      + 0.2 / (Z_eff + 1) * ft34**4
+  )
+
+
+def _calculate_alpha(
+    f_trap: chex.Array,
+    nu_i_star: chex.Array,
+) -> chex.Array:
+  """Calculates the alpha coefficient: Sauter PoP 1999 Eqs. 17a+b."""
+  alpha0 = -1.17 * (1 - f_trap) / (1 - 0.22 * f_trap - 0.19 * f_trap**2)
+  alpha = (
+      (alpha0 + 0.25 * (1 - f_trap**2) * jnp.sqrt(nu_i_star))
+      / (1 + 0.5 * jnp.sqrt(nu_i_star))
+      + 0.315 * nu_i_star**2 * f_trap**6
+  ) / (1 + 0.15 * nu_i_star**2 * f_trap**6)
+  return alpha
