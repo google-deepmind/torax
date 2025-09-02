169ce04531026a9a177556377a16a2b30e689610
MateoBell <mateo.bellouard@gmail.com>
2025-09-02 15:00:40 +0200

    fix test

--- torax/_src/imas_tools/input/tests/core_profiles_test.py
+++ torax/_src/imas_tools/input/tests/core_profiles_test.py
@@ -21,7 +21,7 @@ from absl.testing import parameterized
 import numpy as np
 import torax
 from torax._src.imas_tools.input.core_profiles import core_profiles_from_IMAS
-from torax._src.imas_tools.input.core_profiles import load_profiles_data
+from torax._src.imas_tools.input.core_profiles import load_core_profiles_data
 from torax._src.orchestration.run_simulation import prepare_simulation
 from torax._src.test_utils import sim_test_case
 from torax._src.torax_pydantic import model_config
@@ -46,7 +46,7 @@ class CoreProfilesTest(sim_test_case.SimTestCase):
 
     path = 'core_profiles_ddv4_iterhybrid_rampup_conditions.nc'
     dir = os.path.join(torax.__path__[0], 'data/third_party/imas_data')
-    core_profiles_in = load_profiles_data(path, 'core_profiles', dir)
+    core_profiles_in = load_core_profiles_data(path, 'core_profiles', dir)
 
     # Modifying the input config profiles_conditions class
     core_profiles_conditions = core_profiles_from_IMAS(
@@ -89,7 +89,7 @@ class CoreProfilesTest(sim_test_case.SimTestCase):
         'core_profiles_15MA_DT_50_50_flat_top_slice.nc'
     )
     dir = os.path.join(torax.__path__[0], 'data/third_party/imas_data')
-    core_profiles_in = load_profiles_data(path, 'core_profiles', dir)
+    core_profiles_in = load_core_profiles_data(path, 'core_profiles', dir)
     rhon_in = core_profiles_in.profiles_1d[0].grid.rho_tor_norm
 
     # Modifying the input config profiles_conditions class
