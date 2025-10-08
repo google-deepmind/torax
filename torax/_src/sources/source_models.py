65d81fbcad237d8d3acadac0b2f3c39242ef6825
Ian Goodfellow <goodfellow@google.com>
2025-10-08 09:48:22 -0700

    Fix bug where dataclasses decorator overwrote custom hash
    PiperOrigin-RevId: 816748180

--- torax/_src/sources/source_models.py
+++ torax/_src/sources/source_models.py
@@ -23,7 +23,7 @@ from torax._src.sources import source as source_lib
 
 
 @jax.tree_util.register_dataclass
-@dataclasses.dataclass(frozen=True)
+@dataclasses.dataclass(frozen=True, eq=False)
 class SourceModels:
   """Source models for the different equations being evolved in Torax."""
 
@@ -39,10 +39,21 @@ class SourceModels:
         if source_lib.AffectedCoreProfile.PSI in source.affected_core_profiles
     })
 
-  def __hash__(self) -> int:
+  def _hash(self) -> int:
+    """Custom hash implementation.
+
+    Use this name to distinguish it from __hash__, which may be overwritten
+    inadvertently by multiple inheritance.
+
+    Returns:
+      hash: the hash of this SourceModels
+    """
     hashes = [hash(self.standard_sources)]
     hashes.append(hash(self.qei_source))
     return hash(tuple(hashes))
+
+  def __hash__(self) -> int:
+    return self._hash()
 
   def __eq__(self, other) -> bool:
     if set(self.standard_sources.keys()) == set(other.standard_sources.keys()):
@@ -54,3 +65,10 @@ class SourceModels:
           and self.qei_source == other.qei_source
       )
     return False
+
+  def __post_init__(self):
+    """Run post-init checks."""
+
+    # Make sure our custom hash hasn't been overwritten by the dataclasses
+    # decorator
+    assert hash(self) == self._hash()
