f4d6bbf32e8c390e24a75930157f99daf1db41d6
Ian Goodfellow <goodfellow@google.com>
2025-10-15 12:55:24 -0700

    Cleanup of sawtooth_models
    It wasn't totally clear that SawtoothModels was intended to be a static
    argument because it wasn't frozen.
    I confirm that it is intended to be a static argument: it is a field of
    MHDModels, which is a static field of PhysicsModels.
    
    This CL:
    - Makes SawtoothModels frozen, which is necessary to be a correct
      implementation of a static argument
    - Removes the __hash__ and __eq__ method, because the frozen dataclass
      defaults will work, and the custom __eq__ method here wasn't quite right
      anyway (it assumes that `other` is a dataclass, but `other` can be anything)
    
    PiperOrigin-RevId: 819877395

--- torax/_src/mhd/sawtooth/sawtooth_models.py
+++ torax/_src/mhd/sawtooth/sawtooth_models.py
@@ -19,20 +19,14 @@ from torax._src.mhd.sawtooth import redistribution_base
 from torax._src.mhd.sawtooth import trigger_base
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(frozen=True)
 class SawtoothModels:
-  """Container for sawtooth models."""
+  """Container for sawtooth models.
+
+  This class is intended for use as a static argument to Jax so it is
+  immutable and hashes by value. Because this class is not polymorphic
+  it does not have to hash its class id and can just use the default
+  frozen dataclass hash method.
+  """
   trigger_model: trigger_base.TriggerModel
   redistribution_model: redistribution_base.RedistributionModel
-
-  def __eq__(self, other: 'SawtoothModels') -> bool:
-    return (
-        self.trigger_model == other.trigger_model
-        and self.redistribution_model == other.redistribution_model
-    )
-
-  def __hash__(self) -> int:
-    return hash((
-        self.trigger_model,
-        self.redistribution_model,
-    ))
