Fix bug in `face_grad` on boundaries when taking gradient w.r.t other variable.
At the moment we assume the face values for the other variable can be linearly extrapolated from the cell values but this might not always be the case. e.g. for rmid if the underlying intermediate has greater resolution than our grid resolution this might yield incorrect values.

PiperOrigin-RevId: 854125699

torax/_src/fvm/cell_variable.py
@@ -118,16 +118,22 @@ class CellVariable:
       )
 
   def face_grad(
-      self, x: jt.Float[chex.Array, 'cell'] | None = None
+      self,
+      *,
+      x: jt.Float[chex.Array, 'cell'] | None = None,
+      x_left: jt.Float[chex.Array, ''] | None = None,
+      x_right: jt.Float[chex.Array, ''] | None = None,
   ) -> jt.Float[chex.Array, 'face']:
     """Returns the gradient of this value with respect to the faces.
 
     Implemented using forward differencing of cells. Leftmost and rightmost
-    gradient entries are determined by user specify constraints, see
+    gradient entries are determined by user specified constraints, see
     CellVariable class docstring.
 
     Args:
-      x: (optional) coordinates over which differentiation is carried out
+      x: (optional) coordinates over which differentiation is carried out.
+      x_left: (optional) value of `x` at the leftmost face.
+      x_right: (optional) value of `x` at the rightmost face.
 
     Returns:
       A jax.Array of shape (num_faces,) containing the gradient.
@@ -135,6 +141,8 @@ class CellVariable:
     if x is None:
       forward_difference = jnp.diff(self.value) / jnp.diff(self.cell_centers)
     else:
+      if x_left is None or x_right is None:
+        raise ValueError('Must specify x_left and x_right if x is specified.')
       forward_difference = jnp.diff(self.value) / jnp.diff(x)
 
     def constrained_grad(
@@ -151,10 +159,12 @@ class CellVariable:
               'Cannot constraint both the value and gradient of '
               'a face variable.'
           )
-        coords = self.cell_centers if x is None else x
-        dx = coords[-1] - coords[-2] if right else coords[1] - coords[0]
+        if x is None:
+          dx = self.dr / 2.0
+        else:
+          dx = x_right - x[-1] if right else x[0] - x_left
         sign = -1 if right else 1
-        return sign * (cell - face) / (0.5 * dx)
+        return sign * (cell - face) / dx
       else:
         if grad is None:
           raise ValueError('Must specify one of value or gradient.')

torax/_src/fvm/tests/cell_variable_test.py
@@ -74,7 +74,11 @@ class CellVariableTest(parameterized.TestCase):
         dr=jnp.array(0.1),
     )
 
-    grad = var.face_grad(x=jnp.array([4.0, 1.0, 5.0, 3.0]))
+    grad = var.face_grad(
+        x=jnp.array([4.0, 1.0, 5.0, 3.0]),
+        x_left=jnp.array(5.5),
+        x_right=jnp.array(1.0),
+    )
     np.testing.assert_array_equal(
         grad, jnp.array([0.0, 1.0 / -3.0, 3.0 / 4.0, -2.0 / -2.0, 0.0])
     )

torax/_src/transport_model/qualikiz_based_transport_model.py
@@ -133,6 +133,7 @@ class QualikizBasedTransportModel(
     normalized_logarithmic_gradients = quasilinear_transport_model.NormalizedLogarithmicGradients.from_profiles(
         core_profiles=core_profiles,
         radial_coordinate=rmid,
+        radial_face_coordinate=rmid_face,
         reference_length=geo.R_major,
     )
 
@@ -240,7 +241,9 @@ class QualikizBasedTransportModel(
         left_face_constraint=None,
         left_face_grad_constraint=jnp.array(0.0, dtype=jax_utils.get_dtype()),
     )
-    gamma_E_SI = rmid_face / q * cv.face_grad(rmid)
+    gamma_E_SI = rmid_face / q * cv.face_grad(
+        x=rmid, x_left=rmid_face[0], x_right=rmid_face[-1]
+    )
 
     # We need different normalizations for QuaLiKiz and QLKNN models.
     c_ref = jnp.sqrt(constants.keV_to_J / constants.m_amu)