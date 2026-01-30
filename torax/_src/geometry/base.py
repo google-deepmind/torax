Fix to make validation of face centres consistent and flexible to precision errors
PiperOrigin-RevId: 863198666

torax/_src/geometry/base.py
@@ -59,11 +59,17 @@ class BaseGeometryConfig(torax_pydantic.BaseModelFrozen):
 
     if self.face_centers is not None:
       if len(self.face_centers) < 5:
-        raise ValueError('face_centers must have at least 5 elements (4 cells)')
+        raise ValueError(
+            'face_centers must have at least 5 elements (4 cells). Got'
+            f' {len(self.face_centers)}'
+        )
       if not np.isclose(self.face_centers[0], 0.0) or not np.isclose(
           self.face_centers[-1], 1.0
       ):
-        raise ValueError('face_centers must start at 0.0 and end at 1.0')
+        raise ValueError(
+            'face_centers must start at 0.0 and end at 1.0. Got'
+            f' {self.face_centers}'
+        )
 
     if self.n_rho is not None and self.n_rho < 4:
       raise ValueError('n_rho must be at least 4')

torax/_src/torax_pydantic/interpolated_param_2d.py
@@ -68,9 +68,12 @@ class Grid1D(model_base.BaseModelFrozen):
   def _validate_face_centers(cls, v: np.ndarray) -> np.ndarray:
     """Validates that face_centers has at least 5 elements (4 cells)."""
     if len(v) < 5:
-      raise ValueError('face_centers must have at least 5 elements (4 cells)')
-    if 0.0 not in v or 1.0 not in v:
-      raise ValueError('face_centers must include 0.0 and 1.0')
+      raise ValueError(
+          'face_centers must have at least 5 elements (4 cells) but got'
+          f' {len(v)}'
+      )
+    if not np.isclose(v[0], 0.0) or not np.isclose(v[-1], 1.0):
+      raise ValueError(f'face_centers must include 0.0 and 1.0 but got {v}')
     return v
 
   @property