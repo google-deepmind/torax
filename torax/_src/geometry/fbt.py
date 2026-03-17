Corrected flux surface average of grad psi in FBT geometry.
The calculation for flux_surf_avg_grad_psi was missing a factor of 2 * pi. A new test was added to check the Cauchy-Schwarz inequality, which requires g0^2 / g1 <= 1. This test revealed the missing factor, as the ratio was unphysically small without it.

Only impacts particle transport (convection term), and only when not using DVeff with FBT geometry, which is not the case in any production runs or test cases.

PiperOrigin-RevId: 885188891

torax/_src/geometry/fbt.py
@@ -513,7 +513,7 @@ def _from_fbt(
       flux_surf_avg_1_over_R=LY['Q0Q'],
       flux_surf_avg_1_over_R2=LY['Q2Q'],
       flux_surf_avg_grad_psi2_over_R2=LY['Q3Q'],
-      flux_surf_avg_grad_psi=LY['Q5Q'],
+      flux_surf_avg_grad_psi=2 * np.pi * LY['Q5Q'],
       flux_surf_avg_grad_psi2=LY['Q4Q'],
       flux_surf_avg_B2=flux_surf_avg_B2,
       flux_surf_avg_1_over_B2=flux_surf_avg_1_over_B2,