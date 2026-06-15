Fix sign mismatch for psi and derived quantities

torax/_src/imas_tools/input/equilibrium.py
@@ -106,7 +106,7 @@ def _geometry_from_single_slice(
   # TypeErrors during PyTree tracing.
 
   # Poloidal flux.
-  psi = np.asarray(IMAS_data.profiles_1d.psi)
+  psi = -1 * np.asarray(IMAS_data.profiles_1d.psi)
 
   # Toroidal flux.
   phi = np.asarray(IMAS_data.profiles_1d.phi)
@@ -130,14 +130,14 @@ def _geometry_from_single_slice(
   # Flux surface integrals of various geometry quantities.
   # IDS Contour integrals.
   if IMAS_data.profiles_1d.dvolume_dpsi:
-    dvoldpsi = np.asarray(IMAS_data.profiles_1d.dvolume_dpsi)
+    dvoldpsi = -1 * np.asarray(IMAS_data.profiles_1d.dvolume_dpsi)
   else:
-    dvoldpsi = np.gradient(
+    dvoldpsi = -1 * np.gradient(
         IMAS_data.profiles_1d.volume, IMAS_data.profiles_1d.psi
     )
   # dpsi_drho_tor
   if IMAS_data.profiles_1d.dpsi_drho_tor:
-    dpsidrhotor = np.asarray(IMAS_data.profiles_1d.dpsi_drho_tor)
+    dpsidrhotor = -1 * np.asarray(IMAS_data.profiles_1d.dpsi_drho_tor)
   else:
     rho_tor = IMAS_data.profiles_1d.rho_tor
     if not rho_tor:
@@ -147,7 +147,7 @@ def _geometry_from_single_slice(
             " equilibrium IDS"
         )
       rho_tor = np.sqrt(IMAS_data.profiles_1d.phi / (np.pi * B_0))
-    dpsidrhotor = np.gradient(IMAS_data.profiles_1d.psi, rho_tor)
+    dpsidrhotor = -1 * np.gradient(IMAS_data.profiles_1d.psi, rho_tor)
 
   flux_surf_avg_grad_psi = IMAS_data.profiles_1d.gm7 * dpsidrhotor
   flux_surf_avg_grad_psi2 = IMAS_data.profiles_1d.gm3 * (dpsidrhotor**2)

torax/_src/imas_tools/output/core_profiles.py
@@ -249,9 +249,11 @@ def _fill_profiles_1d_grid(
   ids.profiles_1d[i].grid.rho_tor = np.concatenate(
       [[0.0], geometry_slice.rho, [geometry_slice.rho_b]]
   )
-  ids.profiles_1d[i].grid.psi = cp_state.psi.cell_plus_boundaries()
-  ids.profiles_1d[i].grid.psi_magnetic_axis = cp_state.psi.left_face_value[0]
-  ids.profiles_1d[i].grid.psi_boundary = cp_state.psi.right_face_value[0]
+  ids.profiles_1d[i].grid.psi = -1 * cp_state.psi.cell_plus_boundaries()
+  ids.profiles_1d[i].grid.psi_magnetic_axis = -1 * (
+      cp_state.psi.left_face_value[0]
+  )
+  ids.profiles_1d[i].grid.psi_boundary = -1 * cp_state.psi.right_face_value[0]
   ids.profiles_1d[i].grid.rho_pol_norm = np.sqrt(
       (cp_state.psi.cell_plus_boundaries() - cp_state.psi.left_face_value[0])
       / (cp_state.psi.right_face_value[0] - cp_state.psi.left_face_value[0])