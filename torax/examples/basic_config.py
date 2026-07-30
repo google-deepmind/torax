21cb5b2b8c270b21fd07f2fae974446febf6ac6c
Tamara Norman <tamaranorman@google.com>
2026-07-30 05:19:55 -0700

    Update examples to use combined transport model
    PiperOrigin-RevId: 956457961

--- torax/examples/basic_config.py
+++ torax/examples/basic_config.py
@@ -41,7 +41,10 @@ CONFIG = {
     },
     'pedestal': {},
     'transport': {
-        'model_name': 'constant',
+        'model_name': 'combined',
+        'transport_models': [
+            {'model_name': 'constant'},
+        ],
     },
     'solver': {
         'solver_type': 'linear',
