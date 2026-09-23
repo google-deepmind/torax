df82fe57f54c8870daf531290dcbf4b8d3465515
Jonathan Citrin <citrin@google.com>
2026-09-22 17:31:29 -0700

    Consolidate neoclassical evaluations into single-call NeoclassicalModels.__call__
    - Introduce `NeoclassicalOutputs` and `NeoclassicalModels.__call__` to evaluate all four neoclassical sub-models (`conductivity`, `bootstrap_current`, `transport`, `poloidal_velocity`) in a single call.
    - Pass the resulting precomputed outputs to source and transport coefficient builders instead of calling neoclassical sub-models separately.
    
    PiperOrigin-RevId: 986345805

---
+++ torax/_src/neoclassical/poloidal_velocity/zeros.py
@@ -0,0 +1,56 @@
+# Copyright 2026 DeepMind Technologies Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Zeros model for neoclassical poloidal velocity."""
+from typing import Annotated, Literal
+from torax._src import state
+from torax._src.config import runtime_params as runtime_params_lib
+from torax._src.geometry import geometry as geometry_lib
+from torax._src.neoclassical.poloidal_velocity import base
+from torax._src.neoclassical.poloidal_velocity import runtime_params as poloidal_velocity_runtime_params
+from torax._src.torax_pydantic import torax_pydantic
+
+
+class ZerosModel(base.PoloidalVelocityModel):
+  """Zeros model for neoclassical poloidal velocity."""
+
+  def calculate_poloidal_velocity(
+      self,
+      runtime_params: runtime_params_lib.RuntimeParams,
+      geometry: geometry_lib.Geometry,
+      core_profiles: state.CoreProfiles,
+  ) -> base.PoloidalVelocity:
+    """Returns zero poloidal velocity."""
+    return base.PoloidalVelocity.zeros(geometry)
+
+  def __eq__(self, other) -> bool:
+    return isinstance(other, self.__class__)
+
+  def __hash__(self) -> int:
+    return hash(self.__class__)
+
+
+class ZerosModelConfig(base.PoloidalVelocityModelConfig):
+  """Config for the Zeros model implementation of poloidal velocity."""
+
+  model_name: Annotated[Literal['zeros'], torax_pydantic.JAX_STATIC] = 'zeros'
+
+  def build_runtime_params(
+      self,
+  ) -> poloidal_velocity_runtime_params.RuntimeParams:
+    return poloidal_velocity_runtime_params.RuntimeParams(
+        poloidal_velocity_multiplier=self.poloidal_velocity_multiplier,
+    )
+
+  def build_model(self) -> ZerosModel:
+    return ZerosModel()
