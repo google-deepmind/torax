30bbee17c32dfb142cf45d00e42442a636dfaac9
Jonathan Citrin <citrin@google.com>
2026-09-26 14:38:11 -0700

    Add neoclassical/poloidal_velocity models (base, zeros, kim)
    - Create `torax._src.neoclassical.poloidal_velocity` with `PoloidalVelocity`, `PoloidalVelocityModel`, `PoloidalVelocityModelConfig`, `KimModel` (delegating to `formulas.calculate_poloidal_velocity`), and `ZerosModel`.
    - Add unit tests for `KimModel` and `ZerosModel`.
    
    This PR only introduces the `poloidal_velocity` sub-package and model implementations without modifying existing APIs or call sites. Subsequent PRs in this stack wire `poloidal_velocity` into `Neoclassical` config and `NeoclassicalModels`, store `poloidal_velocity` on `CoreProfiles` for `post_processing` and `physics.rotation`, and move `calculate_poloidal_velocity` from `neoclassical/formulas` into `poloidal_velocity/kim`.
    
    PiperOrigin-RevId: 988938986

---
+++ torax/_src/neoclassical/poloidal_velocity/kim.py
@@ -0,0 +1,77 @@
+# Copyright 2026 DeepMind Technologies Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Kim model for neoclassical poloidal velocity."""
+from typing import Annotated, Literal
+from torax._src import state
+from torax._src.config import runtime_params as runtime_params_lib
+from torax._src.geometry import geometry as geometry_lib
+from torax._src.neoclassical.formulas import formulas
+from torax._src.neoclassical.poloidal_velocity import base
+from torax._src.physics import psi_calculations
+from torax._src.torax_pydantic import torax_pydantic
+
+# pylint: disable=invalid-name
+
+
+class KimModel(base.PoloidalVelocityModel):
+  """Kim (1991) model for neoclassical poloidal velocity."""
+
+  def calculate_poloidal_velocity(
+      self,
+      runtime_params: runtime_params_lib.RuntimeParams,
+      geometry: geometry_lib.Geometry,
+      core_profiles: state.CoreProfiles,
+  ) -> base.PoloidalVelocity:
+    """Calculates poloidal velocity according to the Kim (1991) model."""
+    B_tor_face = geometry.F_face / geometry.R_major_profile_face
+    B_pol_squared_face = psi_calculations.calc_bpol_squared(
+        geometry, core_profiles.psi
+    )
+    B_total_squared_face = B_pol_squared_face + B_tor_face**2
+
+    poloidal_velocity_params = getattr(
+        runtime_params.neoclassical, 'poloidal_velocity', None
+    )
+    multiplier = (
+        poloidal_velocity_params.poloidal_velocity_multiplier
+        if poloidal_velocity_params is not None
+        else 1.0
+    )
+    v_pol = formulas.calculate_poloidal_velocity(
+        T_i=core_profiles.T_i,
+        n_i=core_profiles.n_i.face_value(),
+        q=core_profiles.q_face,
+        Z_eff=core_profiles.Z_eff_face,
+        Z_i=core_profiles.Z_i_face,
+        B_tor=B_tor_face,
+        B_total_squared=B_total_squared_face,
+        geo=geometry,
+        poloidal_velocity_multiplier=multiplier,
+    )
+    return base.PoloidalVelocity(v_pol=v_pol)
+
+  def __eq__(self, other) -> bool:
+    return isinstance(other, self.__class__)
+
+  def __hash__(self) -> int:
+    return hash(self.__class__)
+
+
+class KimModelConfig(base.PoloidalVelocityModelConfig):
+  """Config for the Kim (1991) model implementation of poloidal velocity."""
+
+  model_name: Annotated[Literal['kim'], torax_pydantic.JAX_STATIC] = 'kim'
+
+  def build_model(self) -> KimModel:
+    return KimModel()
